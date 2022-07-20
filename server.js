16562f8105ac52d66e98583ac0a0b604042c50f2
Abhishek Kumar <abhishekpawan297@gmail.com>
2022-07-21 03:38:52 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require("path")
+const express = require("express");
+const cors = require("cors")
+const dotenv = require("dotenv").config();
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000;
+
+const app = express();
+connectDB()
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
